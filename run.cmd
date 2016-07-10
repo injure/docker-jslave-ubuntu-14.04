8825aa84254ba881c38db3fb97b0eb09966fc5ef
Injure Hu <injurehu@gmail.com>
2016-07-11 00:31:42 +0800

    remove -v in run.cmd

--- run.cmd
+++ run.cmd
@@ -1,6 +1,6 @@
-docker run -idt --name jslave-mega -v ~/docker/volumes/jslave-mega:/home/jenkins \
--e JENKINS_HOST="docker.injure.hu" \
--e JENKINS_PORT="8080" \
--e AGENT_TOKEN="e139b67e100481135d474ca9f4bc13892b5efa5487974221bfc68bc7016f376f" \
--e AGENT_NAME="mega" \
-jslave:mega
+docker run -idt --name jslave-mi \
+-e JENKINS_HOST="<jenkins_host>" \
+-e JENKINS_PORT="<jenkins_port>" \
+-e AGENT_TOKEN="<token>" \
+-e AGENT_NAME="<agent_name>" \
+<image>:<tag>
